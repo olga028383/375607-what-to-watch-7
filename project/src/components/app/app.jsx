bbf4a2df6e0d49a3aebf2f72359fc9b4abb15f46
Olga <olga028383@mail.ru>
2021-06-11 22:00:34 +0300

    Routing

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import Home from '../home/home';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../constants.js';
+import Home from '../pages/home/home';
+import MyList from '../pages/my-list/my-list';
+import FilmDetail from '../pages/film-detail/film-detail';
+import SingIn from '../pages/sing-in/sing-in';
+import Review from '../pages/review/review';
+import Player from '../pages/player/player';
+import NotFound from '../not-found/not-found';
 
 function App() {
   return (
-    <Home
-      name={'The Grand Budapest Hotel'}
-      genre={'Drama'}
-      date={2014}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.LOGIN}>
+          <SingIn/>
+        </Route>
+        <Route exact path={AppRoute.ROOT}>
+          <Home
+            name={'The Grand Budapest Hotel'}
+            genre={'Drama'}
+            date={2014}
+          />
+        </Route>
+        <Route exact path={AppRoute.MY_LIST}>
+          <MyList/>
+        </Route>
+        <Route exact path={AppRoute.FILM_DETAIL}>
+          <FilmDetail/>
+        </Route>
+        <Route exact path={AppRoute.REVIEW}>
+          <Review/>
+        </Route>
+        <Route exact path={AppRoute.PLAYER}>
+          <Player/>
+        </Route>
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/constants.js
@@ -0,0 +1,10 @@
+const AppRoute = {
+  LOGIN: '/login',
+  ROOT: '/',
+  MY_LIST: '/mylist',
+  FILM_DETAIL: '/films/:id',
+  REVIEW: '/films/:id/review',
+  PLAYER: '/player/:id',
+};
+
+export {AppRoute};
