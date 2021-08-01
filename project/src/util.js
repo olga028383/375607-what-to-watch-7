ec42c98a71334aae8b51307b52ba390ac5daac50
Olga <olga028383@mail.ru>
2021-08-01 12:41:10 +0300

    test

--- project/src/util.js
+++ project/src/util.js
@@ -1,7 +1,10 @@
-import {MIN_TIME, ALL_GENRES, AuthorizationStatus} from './constants.js';
-
+import {MIN_TIME, ALL_GENRES, AuthorizationStatus, TEN, MIN_LENGTH_COMMENT, MAX_LENGTH_COMMENT} from './constants.js';
 
 const getLengthTimeFormat = (numeric) => {
+  if (!numeric) {
+    return '';
+  }
+
   const minutes = numeric % MIN_TIME;
   const hours = (numeric - minutes) / MIN_TIME;
 
@@ -16,11 +19,15 @@ const getLengthTimeFormat = (numeric) => {
   return result.trim();
 };
 
-const getUnitTimeFormat = (unit) => unit < 10 ? `0${unit}` : unit;
+const getUnitTimeFormat = (unit) => unit < TEN ? `0${unit}` : `${unit}`;
 
 const getLengthTimeVideoFormat = (numeric) => {
+  if (!numeric) {
+    return '';
+  }
+
   const hours = Math.floor(numeric / (MIN_TIME * MIN_TIME));
-  const minutes = Math.floor(numeric / MIN_TIME);
+  const minutes = Math.floor(numeric / MIN_TIME - hours);
   const seconds = Math.floor(numeric - minutes * 60);
 
   const hoursValue = getUnitTimeFormat(hours);
@@ -42,4 +49,6 @@ const isCheckAuth = (authorizationStatus) => authorizationStatus === Authorizati
 
 const isValidateEmail = (email) => /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(email);
 
-export {getLengthTimeFormat, getFilterFilms, isCheckAuth, isValidateEmail, getLengthTimeVideoFormat};
+const checkLengthReview = (comment) => comment.length > MIN_LENGTH_COMMENT && comment.length <= MAX_LENGTH_COMMENT;
+
+export {getLengthTimeFormat, getFilterFilms, isCheckAuth, isValidateEmail, getLengthTimeVideoFormat, getUnitTimeFormat, checkLengthReview};
